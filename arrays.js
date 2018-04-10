64cc7ed241963c87405c2d3ca66b835232b358f6
Catherine Lee <lee.catherine77@gmail.com>
2018-04-10 14:59:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array
 }
 
-function addElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
