27648cd3bcd57f4e2d79252774b3fb9c04532a95
Catherine Lee <lee.catherine77@gmail.com>
2018-04-10 14:47:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   var newArr = [element, ...array];
+  return newArr
 }
